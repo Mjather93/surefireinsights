29d8a6e4ef846e591b89978739dceaa32c4f8256
Mjather93 <matt.atherton1@live.com>
2024-01-23 17:08:42 +0000

    split run_system_specs.py to add system_specs.py

---
+++ system_specs.py
@@ -0,0 +1,80 @@
+import platform
+import cpuinfo
+import psutil
+import re
+import socket
+import uuid
+from datetime import datetime
+
+
+def get_size(memorysize, suffix="B"):
+    """
+    Scale bytes to its proper format
+    e.g:
+        1253656 => '1.20MB'
+        1253656678 => '1.17GB'
+    """
+    factor = 1024
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if memorysize < factor:
+            return f"{memorysize:.2f}{unit}{suffix}"
+        memorysize /= factor
+
+
+def system_information():
+    print("=" * 40, "System Information", "=" * 40)
+    uname = platform.uname()
+    print(f"Node Name: {uname.node}")
+    print(f"System: {uname.system}")
+    print(f"Release: {uname.release}")
+    print(f"Version: {uname.version}")
+    print(f"Machine: {uname.machine}")
+    print(f"Processor: {uname.processor}")
+    print(f"Processor: {cpuinfo.get_cpu_info()['brand_raw']}")
+    print(f"Ip-Address: {socket.gethostbyname(socket.gethostname())}")
+    print(f"Mac-Address: {':'.join(re.findall('..', '%012x' % uuid.getnode()))}")
+
+    # Boot Time
+    print("=" * 40, "Boot Time", "=" * 40)
+    boot_time_timestamp = psutil.boot_time()
+    bt = datetime.fromtimestamp(boot_time_timestamp)
+    print(f"Boot Time: {bt.year}/{bt.month}/{bt.day} {bt.hour}:{bt.minute}:{bt.second}")
+
+    # print CPU information
+    print("=" * 40, "CPU Info", "=" * 40)
+    # number of cores
+    print("Physical cores:", psutil.cpu_count(logical=False))
+    print("Total cores:", psutil.cpu_count(logical=True))
+    # CPU frequencies
+    cpufreq = psutil.cpu_freq()
+    print(f"Max Frequency: {cpufreq.max:.2f}Mhz")
+
+    # Memory Information
+    print("=" * 40, "Memory Information", "=" * 40)
+    # get the memory details
+    svmem = psutil.virtual_memory()
+    print(f"Total: {get_size(svmem.total)}")
+
+    # Disk Information
+    print("=" * 40, "Disk Information", "=" * 40)
+    print("Partitions and Usage:")
+    # get all disk partitions
+    partitions = psutil.disk_partitions()
+    for partition in partitions:
+        print(f"=== Device: {partition.device} ===")
+        print(f"  Mountpoint: {partition.mountpoint}")
+        print(f"  File system type: {partition.fstype}")
+        try:
+            partition_usage = psutil.disk_usage(partition.mountpoint)
+        except PermissionError:
+            # this can be catched due to the disk that
+            # isn't ready
+            continue
+        print(f"  Total Size: {get_size(partition_usage.total)}")
+        print(f"  Used: {get_size(partition_usage.used)}")
+        print(f"  Free: {get_size(partition_usage.free)}")
+        print(f"  Percentage: {partition_usage.percent}%")
+
+
+if __name__ == "__main__":
+    system_information()
