b79a24f1c873bcb89b400c8074011be3e038bae9
Mjather93 <matt.atherton1@live.com>
2024-01-25 17:30:59 +0000

    updated charting to inlucde multiple types of charts

--- generate_chart.py
+++ generate_chart.py
@@ -18,6 +18,7 @@ connection = sqlite3.connect('surefireinsights.db')
 for chart_config in config.get('charts', []):
     chart_name = chart_config.get('chart_name')
     chart_query = chart_config.get('chart_query')
+    chart_type = chart_config.get('chart_type')
     x_column_name = chart_config.get('x_column_name')
     y_column_name = chart_config.get('y_column_name')
     chart_title = chart_config.get('chart_title')
@@ -33,7 +34,14 @@ for chart_config in config.get('charts', []):
     df = pd.DataFrame(data, columns=[x_column_name, y_column_name])
 
     # Create an interactive scatter plot
-    fig = px.line(df, x=x_column_name, y=y_column_name, title=chart_title, template=template)
+    if chart_type == 'line':
+        fig = px.line(df, x=x_column_name, y=y_column_name, title=chart_title, template=template)
+    elif chart_type == 'bar':
+        fig = px.bar(df, x=x_column_name, y=y_column_name, title=chart_title, template=template)
+    elif chart_type == 'pie':
+        fig = px.pie(df, names=x_column_name, values=y_column_name, title=chart_title, template=template)
+    else:
+        raise ValueError("Invalid chart type")
 
     # Save the Plotly figure to an HTML file
     fig.write_html(f'reports/{chart_name}.html')
