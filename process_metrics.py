e2ff2da43df0b40141d663a188f8772c59f3efe1
dan-corless1994 <daniel_corless94@live.com>
2024-01-25 18:07:59 +0000

    added to main.py

--- process_metrics.py
+++ process_metrics.py
@@ -1,4 +1,5 @@
 import csv
+import sqlite3
 from datetime import datetime
 import psutil
 
@@ -10,7 +11,7 @@ class ProcessMetrics:
     """
 
     @staticmethod
-    def get_process_metrics():
+    def get_process_metrics(names_to_match):
         """
         Public function
 
@@ -21,33 +22,34 @@ class ProcessMetrics:
         processes_data = {}
         process_metrics = psutil.process_iter()
         for processes in process_metrics:
+            if not processes.name() in names_to_match:
+                continue
             processes_info = {
-                'timestamp': datetime.now(),
+                'timestamp': f'{datetime.now()}',
                 'process_id': processes.pid,
                 'process_name': processes.name(),
-                'cpu_percent': processes.cpu_percent(interval=None),
-                'memory_percent': processes.memory_percent(),
-                'num_of_handles': processes.num_handles(),
-                'io_read_count': processes.io_counters().read_count,
-                'io_write_count': processes.io_counters().write_count
+                'process_cpu_percent': processes.cpu_percent(interval=None),
+                'process_memory_percent': processes.memory_percent(),
+                'process_num_of_handles': processes.num_handles(),
+                'process_read_count': processes.io_counters().read_count,
+                'process_write_count': processes.io_counters().write_count
             }
             try:
-                processes_info['command_line'] = processes.cmdline()
+                processes_info['process_command_line'] = processes.cmdline()
             except Exception as e:
-                processes_info['command_line'] = ''
+                processes_info['process_command_line'] = ''
             try:
-                processes_info['num_of_threads'] = processes.threads()
+                processes_info['process_num_of_threads'] = processes.threads()
             except Exception as e:
-                processes_info['num_of_threads'] = 0
+                processes_info['process_num_of_threads'] = 0
             try:
-                processes_info['num_of_open_files'] = processes.open_files()
+                processes_info['process_num_of_open_files'] = processes.open_files()
             except Exception as e:
-                processes_info['num_of_open_files'] = 0
+                processes_info['process_num_of_open_files'] = 0
 
             processes_data[processes.pid] = processes_info
         return processes_data
 
-    # we should change these to insert into the SQLite DB
     @staticmethod
     def write_to_csv(metrics, csv_file):
         with open(csv_file, 'a', newline='') as csvfile:
@@ -62,3 +64,32 @@ class ProcessMetrics:
             csv_file):
         metrics = ProcessMetrics.get_process_metrics()
         ProcessMetrics.write_to_csv(metrics, csv_file)
+
+    @staticmethod
+    def write_to_db(
+            metrics,
+            report_fk
+    ):
+        # print(tuple(metrics.values()))
+        for processes in metrics.values():
+            print(processes.values())
+            connection = sqlite3.connect('surefireinsights.db')
+            cursor = connection.cursor()
+            # return metrics
+            cursor.execute('''
+                INSERT INTO process_metrics
+                (timestamp, process_id, process_name, process_command_line, process_cpu_percent, process_memory_percent,
+                process_num_of_threads, process_num_of_handles, process_num_of_open_files, process_read_count,
+                process_write_count, report_fk)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                ''', tuple(processes.values()) + (report_fk,))
+            connection.commit()
+            connection.close()
+
+    @staticmethod
+    def collect_and_write_process_metrics_to_db(
+            report_fk,
+            names_to_match
+    ):
+        metrics = ProcessMetrics.get_process_metrics(names_to_match)
+        ProcessMetrics.write_to_db(metrics, report_fk=report_fk)
--- run_process_metrics.py
+++ run_process_metrics.py
@@ -4,20 +4,32 @@
  Version: 0.0.1
  https://psutil.readthedocs.io/en/latest
 """
+import argparse
+import csv
+import sqlite3
 import time
 from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
 import process_metrics
+from get_report_fk import report_fk
 
+
+argParser = argparse.ArgumentParser()
+argParser.add_argument("-ntm", "--name_to_match", help="list of names to match for process metrics")
+args = argParser.parse_args()
 
 csv_file_path = 'C:/Users/corlessd/OneDrive - ESG/Python/surefireinsights/process_metrics.csv'
 interval_seconds = 5
+name_to_match = 'java|erlsv|sql'
 
 try:
     # execute the collection with multiple threads to speed up the process.
-    with ThreadPoolExecutor(max_workers=1) as executor:
+    with ThreadPoolExecutor(max_workers=2) as executor:
         while True:
-            executor.submit(process_metrics.ProcessMetrics.collect_and_write_process_metrics(csv_file_path))
+            executor.submit(process_metrics.ProcessMetrics.collect_and_write_process_metrics_to_db(report_fk=report_fk,
+                                                                                                   names_to_match=name_to_match))
             time.sleep(interval_seconds)
+
 # prints a new error message if you cancel the script.
 except KeyboardInterrupt:
-    print("Monitoring stopped. Exported metrics to", csv_file_path)
+    print("Monitoring stopped.")
