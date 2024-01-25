ec2e013f74ea1727515df9a06c9a299e2a0d0b6a
dan-corless1994 <daniel_corless94@live.com>
2024-01-25 10:22:05 +0000

    added to main.py

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
 import sqlite3
 import subprocess
+import datetime
+import os
+import pathlib
+import task_executor
+import yaml
+import logging.config
+from concurrent.futures import ThreadPoolExecutor
 
+
+def load_conf_file(file_path):
+    with open(file_path, "r") as f:
+        config = yaml.safe_load(f)
+        return config
+
+
+working_dir = pathlib.Path().resolve()
+config_file = str(working_dir) + "/config/conf.yaml"
+log_config_file = str(working_dir) + "/config/logging.conf"
+logfile = str(working_dir) + '/logs/system.log'
+logdir = os.path.dirname(logfile)
+if not pathlib.Path(logdir).exists():
+    try:
+        os.makedirs(logdir)
+        print("Created directory '%s'" % logdir)
+    except OSError as error:
+        logging.critical("Exception occurred", exc_info=True)
+log = open(logfile, "a")
+log.close()
+logging.config.fileConfig(fname=log_config_file, disable_existing_loggers=False,
+                          defaults={'logfilename': logfile.replace("\\", "/")})
+logging.info('test.py has initialised.')
+scripts = load_conf_file(file_path=config_file)
 
 # If it doesn't already exist, create the sqlite database
 subprocess.run(["python", "create_sqlite_db.py"])
@@ -11,7 +42,7 @@ monitoring_duration = input("How long would you like monitoring to run for, in s
 
 # Input configuration items into the database
 subprocess.run(["python", "configure_monitoring.py", '--report_name', report_name,
-               '--monitoring_duration', monitoring_duration])
+                '--monitoring_duration', monitoring_duration])
 
 # Extract the report_pk for this report and store in a variable
 # Insert the report and monitoring info into the report table of the sqlite db
@@ -25,5 +56,19 @@ connection.close()
 
 subprocess.run(["python", "run_system_specs.py", '--report_pk', str(report_pk)])
 
+time_delta = datetime.timedelta(seconds=float(monitoring_duration))
+end_time = (datetime.datetime.now() + datetime.timedelta(seconds=float(monitoring_duration)))
+print(end_time)
 
+print(scripts)
 
+for script in scripts['scripts']:
+    logging.info(script)
+    task_to_do = task_executor.ExecuteTasks.execute_script(script)
+    with ThreadPoolExecutor(max_workers=script['threads']) as executor:
+        finished = False
+        while not finished:
+            current_time = datetime.datetime.now()
+            if current_time >= end_time:
+                finished = True
+                break
