ceb70eedad63479eda69c02bd4c321c85e569ca4
dan-corless1994 <daniel_corless94@live.com>
2024-01-26 16:31:16 +0000

    Got the process_metrics working and into a structured class with methods. Committing this so I can pull the main version

--- process_metrics.py
+++ process_metrics.py
@@ -1,95 +1,129 @@
-import csv
-import sqlite3
-from datetime import datetime
+import datetime
+import json
+import time
 import psutil
+import multiprocessing
+import sqlite3
 
 
-class ProcessMetrics:
+class ProcessPerformanceExtractor:
     """
-    This class respresents a collection of methods to gather system-level metrics
-    using the psutil library.
+    Description:
+        A class to extract process-level performance metrics.
+        We use a multiprocessing pool to manage the processing of process-level metrics
+        across multiple threads.
+        Each metric has a method for simplicity.
     """
 
+    def __init__(self, criteria):
+        self.criteria = criteria
+        self.last_io_counters = {}
+
+    def get_matching_process_ids(self):
+        # method to find process IDs that match our process name criteria.
+        # we store them in a list to multiprocess the process Ids
+        matching_ids = []
+        for process in psutil.process_iter(['pid', 'name', 'cmdline']):
+            if self.criteria.lower() in process.info['name'].lower():
+                matching_ids.append(process.info)
+        return matching_ids
+
+    # we need to JSONIFY the cmdline data to store it in the DB.
+    def cmdline_to_json(self, cmdline):
+        return json.dumps(cmdline)
+
+    def get_current_time(self):
+        return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    def get_cpu_usage(self, process_info):
+        try:
+            process = psutil.Process(process_info['pid'])
+            cpu_percent = process.cpu_percent(interval=1)
+            return (process_info['pid'], self.get_current_time(), process_info['name'],
+                    self.cmdline_to_json(process_info['cmdline']), "cpu_usage_percent", cpu_percent)
+        except psutil.NoSuchProcess:
+            return (process_info['pid'], self.get_current_time(), process_info['name'],
+                    self.cmdline_to_json(process_info['cmdline']), "cpu_usage_percent", 0)
+
+    def get_memory_usage(self, process_info):
+        try:
+            process = psutil.Process(process_info['pid'])
+            memory_info = process.memory_info()
+            return (process_info['pid'], self.get_current_time(),
+                    process_info['name'],
+                    self.cmdline_to_json(process_info['cmdline']), "memory_usage_mb", memory_info.rss / (1024 ** 2))
+        except psutil.NoSuchProcess:
+            return (process_info['pid'], self.get_current_time(),
+                    process_info['name'],
+                    self.cmdline_to_json(process_info['cmdline']), "memory_usage_mb", 0)
+
+    def get_io_read_bytes(self, process_info):
+        try:
+            process = psutil.Process(process_info['pid'])
+            io_counters = process.io_counters()
+            return (process_info['pid'], self.get_current_time(),
+                    process_info['name'],
+                    self.cmdline_to_json(process_info['cmdline']), "read_bytes", io_counters.read_bytes)
+        except psutil.NoSuchProcess:
+            return (process_info['pid'], self.get_current_time(),
+                    process_info['name'],
+                    self.cmdline_to_json(process_info['cmdline']), "read_bytes", 0)
+
+    def get_io_read_count(self, process_info):
+        try:
+            process = psutil.Process(process_info['pid'])
+            io_counters = process.io_counters()
+            return (process_info['pid'], self.get_current_time(),
+                    process_info['name'],
+                    self.cmdline_to_json(process_info['cmdline']), "read_count", io_counters.read_count)
+        except psutil.NoSuchProcess:
+            return (process_info['pid'], self.get_current_time(),
+                    process_info['name'],
+                    self.cmdline_to_json(process_info['cmdline']), "read_count", 0)
+
+    def get_io_write_bytes(self, process_info):
+        try:
+            process = psutil.Process(process_info['pid'])
+            io_counters = process.io_counters()
+            return (process_info['pid'], self.get_current_time(),
+                    process_info['name'],
+                    self.cmdline_to_json(process_info['cmdline']), "write_bytes", io_counters.write_bytes)
+        except psutil.NoSuchProcess:
+            return (process_info['pid'], self.get_current_time(),
+                    process_info['name'],
+                    self.cmdline_to_json(process_info['cmdline']), "write_bytes", 0)
+
+    def get_io_write_count(self, process_info):
+        try:
+            process = psutil.Process(process_info['pid'])
+            io_counters = process.io_counters()
+            return (process_info['pid'], self.get_current_time(),
+                    process_info['name'],
+                    self.cmdline_to_json(process_info['cmdline']), "write_count", io_counters.write_count)
+        except psutil.NoSuchProcess:
+            return (process_info['pid'], self.get_current_time(),
+                    process_info['name'],
+                    self.cmdline_to_json(process_info['cmdline']), "write_count", 0)
+
     @staticmethod
-    def get_process_metrics(names_to_match):
-        """
-        Public function
-
-        Created on: 23-01-2024
-        Description: A method that collects system level performance metrics.
-        :return:
-        """
-        processes_data = {}
-        process_metrics = psutil.process_iter()
-        for processes in process_metrics:
-            if not processes.name() in names_to_match:
-                continue
-            processes_info = {
-                'timestamp': f'{datetime.now()}',
-                'process_id': processes.pid,
-                'process_name': processes.name(),
-                'process_cpu_percent': processes.cpu_percent(interval=None),
-                'process_memory_percent': processes.memory_percent(),
-                'process_num_of_handles': processes.num_handles(),
-                'process_read_count': processes.io_counters().read_count,
-                'process_write_count': processes.io_counters().write_count
-            }
-            try:
-                processes_info['process_command_line'] = processes.cmdline()
-            except Exception as e:
-                processes_info['process_command_line'] = ''
-            try:
-                processes_info['process_num_of_threads'] = processes.threads()
-            except Exception as e:
-                processes_info['process_num_of_threads'] = 0
-            try:
-                processes_info['process_num_of_open_files'] = processes.open_files()
-            except Exception as e:
-                processes_info['process_num_of_open_files'] = 0
-
-            processes_data[processes.pid] = processes_info
-        return processes_data
+    def collect_metrics(process_info, criteria):
+        extractor = ProcessPerformanceExtractor(criteria)
+        cpu_usage = extractor.get_cpu_usage(process_info)
+        memory_usage = extractor.get_memory_usage(process_info)
+        read_bytes = extractor.get_io_read_bytes(process_info)
+        read_count = extractor.get_io_read_count(process_info)
+        write_bytes = extractor.get_io_write_bytes(process_info)
+        write_count = extractor.get_io_write_count(process_info)
+        return [cpu_usage, memory_usage, read_bytes, read_count, write_bytes, write_count]
 
     @staticmethod
-    def write_to_csv(metrics, csv_file):
-        with open(csv_file, 'a', newline='') as csvfile:
-            fieldnames = metrics.keys()
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            if csvfile.tell() == 0:
-                writer.writeheader()
-            writer.writerow(metrics)
+    def write_to_database(data):
+        conn = sqlite3.connect('performance_data.db')
+        cursor = conn.cursor()
 
-    @staticmethod
-    def collect_and_write_process_metrics(
-            csv_file):
-        metrics = ProcessMetrics.get_process_metrics()
-        ProcessMetrics.write_to_csv(metrics, csv_file)
+        for item in data:
+            cursor.execute("INSERT INTO process_metrics VALUES (?, ?, ?, ?, ?, ?)", tuple(item))
 
-    @staticmethod
-    def write_to_db(
-            metrics,
-            report_fk
-    ):
-        # print(tuple(metrics.values()))
-        for processes in metrics.values():
-            print(processes.values())
-            connection = sqlite3.connect('surefireinsights.db')
-            cursor = connection.cursor()
-            # return metrics
-            cursor.execute('''
-                INSERT INTO process_metrics
-                (timestamp, process_id, process_name, process_command_line, process_cpu_percent, process_memory_percent,
-                process_num_of_threads, process_num_of_handles, process_num_of_open_files, process_read_count,
-                process_write_count, report_fk)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-                ''', tuple(processes.values()) + (report_fk,))
-            connection.commit()
-            connection.close()
+        conn.commit()
+        conn.close()
 
-    @staticmethod
-    def collect_and_write_process_metrics_to_db(
-            report_fk,
-            names_to_match
-    ):
-        metrics = ProcessMetrics.get_process_metrics(names_to_match)
-        ProcessMetrics.write_to_db(metrics, report_fk=report_fk)
